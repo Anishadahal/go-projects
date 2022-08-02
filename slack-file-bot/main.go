2e1d4853e49201b933e1f80df5155ff45cee14b7
Anisha Dahal <anisha.dahal@webo.digital>
2022-08-02 09:43:44 +0545

    file upload

--- slack-file-bot/main.go
+++ slack-file-bot/main.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/slack-go/slack"
 )
 
 func main() {
-	
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3879019227168-3879459446579-ChyVM9NsYMwcS7Ac27Hok23n")
+	os.Setenv("CHANNEL_ID", "C03QYKT8FKQ")
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{""}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
 }
